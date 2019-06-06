b9846e0cfbc335f8cc49ac52660ce3833ab92226
ijelynsaele.com <ijelyn@saele.com>
2019-06-06 23:34:20 +0200

    T

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const path = require("path");
 const PORT = process.env.PORT || 8000;
 
 express()
-    .use(express.static(path.join(__dirname, "public")))
-    .set("views", path.join(__dirname, "views"))
-    .set("view engine", "ejs")
-    .get("/", (req, res) => res.render("index"))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+  .use(express.static(path.join(__dirname, "stylesheets")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("index"))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
